6dda406010c92805f1859d36fe4c6f8db78689d8
brian-mokaya <brianmokaya507@gmail.com>
2025-02-25 20:39:56 +0300

    guess game 4th commit

--- GuessingGame/GuessingGame.java
+++ GuessingGame/GuessingGame.java
@@ -1,6 +1,7 @@
-package GuessingGame;
+package  GuessingGame;
 
-import java.util.Scanner;
+import  java.util.Scanner;
+
 public class GuessingGame {
 
     public void startGame(){
@@ -11,12 +12,12 @@ public class GuessingGame {
     boolean GuessedCorrect = false;
     Scanner scanner = new Scanner(System.in);
 
-    system.out.println("Welome to the Guess Game!");
+    System.out.println("Welome to the Guess Game!");
     System.out.println("I have chosen a number between 1 and 100, try to guess it.");
 
     //loop until correct guess
     while(!GuessedCorrect){
-        system.out.println("Enter your guess: ");
+        System.out.println("Enter your guess: ");
         int guess = scanner.nextInt();
         attempts++;
 
@@ -25,11 +26,11 @@ public class GuessingGame {
             System.out.println("Congratulations! You guessed correctly in " + attempts + " attempts!");
             GuessedCorrect = true;
     }
-        else if (guess<randomNumber){
-            system.out.println("The number is high than your guess. Try again.");
+        else if (guess>randomNumber){
+            System.out.println("The number is high than your guess. Try again.");
         }
         else {
-            system.out.println("The number is lower than your guess. Try again.");
+            System.out.println("The number is lower than your guess. Try again.");
         }
     }
     scanner.close();
